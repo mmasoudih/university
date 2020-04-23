544fe612127e60750ad46a996e7e1c916f394d27
Mohamad Masoudi <mohammad.masoudi59@gmail.com>
2020-04-23 11:36:10 +0430

    add something ...

--- src/app.js
+++ src/app.js
@@ -10,14 +10,35 @@ callAllFunc();
 
 function callAllFunc() {
   setToday();
+  showOverlay();
+  hide();
+  show();
+  hideOverlay();
   setLession(moment().day());
   copyToClipBoard(document.querySelectorAll('.link'));
   showInfoAlert();
   showAlert();
   showFooter();
   showTime();
+  
 }
-
+function hideOverlay(){
+  document.querySelector('.return-back').addEventListener('click', e =>{
+    e.preventDefault();
+    hide();
+  });
+}
+function hide(){
+  document.querySelector('.overlay').classList.add('hide');
+  document.querySelector('.overlay').classList.remove('show');
+}
+function show(){
+  document.querySelector('.show-all').addEventListener('click', e =>{
+    e.preventDefault();
+    document.querySelector('.overlay').classList.remove('hide');
+    document.querySelector('.overlay').classList.add('show');
+  });
+}
 function showTime() {
   setInterval(() => {
     document.querySelector('.time').innerHTML = `${moment().format('HH:mm:ss')}`;
@@ -178,38 +199,46 @@ function setLession(day) {
 
 
   // ساعات شنبه 
-  if ((hour >= 11) && (hour <= 14 && minute <= 30)) {
-    setOnline(li, 'web');
-  }
-  if ((hour >= 15 && minute <= 30) && (hour <= 18 && minute <= 30)) {
-    setOnline(li, 'programming');
+  if (day == 6) {
+    if ((hour >= 11) && (hour <= 14 && minute <= 30)) {
+      setOnline(li, 'web');
+    }
+    if ((hour >= 15 && minute <= 30) && (hour <= 18 && minute <= 30)) {
+      setOnline(li, 'programming');
+    }
   }
 
   // ساعات یک‌شنبه 
-  if ((hour >= 11) && (hour <= 13)) {
-    setOnline(li, 'database');
+  if (day == 0) {
+    if ((hour >= 11) && (hour <= 13)) {
+      setOnline(li, 'database');
+    }
   }
 
   // ساعات سه شنبه 
-  if ((hour >= 13) && (hour <= 15)) {
-    setOnline(li, 'math');
-  }
-  if ((hour >= 16) && (hour <= 17)) {
-    setOnline(li, 'work');
-  }
-  if ((hour >= 17) && (hour <= 18 && minute <= 30)) {
-    setOnline(li, 'english');
+  if (day == 2) {
+    if ((hour >= 13) && (hour <= 15)) {
+      setOnline(li, 'math');
+    }
+    if ((hour >= 16) && (hour <= 17)) {
+      setOnline(li, 'work');
+    }
+    if ((hour >= 17) && (hour <= 18 && minute <= 30)) {
+      setOnline(li, 'english');
+    }
   }
 
   // ساعات پنجشنبه 
-  if ((hour >= 10) && (hour <= 13)) {
-    setOnline(li, 'application_programming');
-  }
+  if (day == 4) {
+    if ((hour >= 10) && (hour <= 13)) {
+      setOnline(li, 'application_programming');
+    }
 
-  if ((hour >= 16) && (hour <= 18 && minute <= 0)) {
-    setOnline(li, 'graphic');
+    if ((hour >= 16) && (hour <= 18 && minute <= 0)) {
+      setOnline(li, 'graphic');
+    }
   }
-
+  console.log(day);
 
 }
 
@@ -242,6 +271,88 @@ function copyToClipBoard(a) {
       });
     });
   });
+}
+
+function showOverlay() {
+  let time = 'ساعت';
+  let goToClass = 'کپی لینک کلاس';
+  getElement('.overlay h1', 'برنامه کل هفته');
+  document.querySelector('.overlay ul').innerHTML = `
+  <p>
+  &#8659;
+    شنبه 
+  &#8659;
+  </p>
+  <li id='web'>
+    <span>طراحی وب</span>
+    <span>رضا ابراهیم زاده</span>
+    <span>${time} ۱۴:۳۰−۱۱</span>
+    <span><a href='http://79.143.84.27/web1/' class='link'>برو تو کلاس</a></span>
+  </li>
+  <li id='programming'>
+    <span>برنامه نویسی ۱</span>
+    <span>مهدی صادق زاده</span>
+    <span>${time} ۱۸:۳۰-۱۵:۳۰</span>
+    <span><a href='http://79.143.84.27/programming/' class='link'>برو تو کلاس</a></span>
+  </li>
+  <p>
+  &#8659;
+  یک‌شنبه
+  &#8659;
+  </p>
+  
+  <li id='database'>
+    <span>پایگاه داده</span>
+    <span>حجت بیک نژاد</span>
+    <span>${time} ۱۳−۱۱</span>
+    <span><a href='http://79.143.84.27/databe/' class='link'>${goToClass}</a></span>
+  </li>
+  <p>
+  &#8659;
+  سه‌شنبه
+  &#8659;
+  </p>
+
+  <li id='math'>
+    <span>ریاضی</span>
+    <span>رضا باغبانی</span>
+    <span>${time} ۱۵−۱۳</span>
+    <span><a href='http://79.143.84.27/mathematics/' class='link'>${goToClass}</a></span>
+  </li>
+  <li id='work'>
+    <span>کار افرینی</span>
+    <span>رضا باغبانی</span>
+    <span>${time} ۱۷−۱۶</span>
+    <span><a href='http://79.143.84.27/work/' class='link'>${goToClass}</a></span>
+  </li>
+  <li id='english'>
+    <span>زبان تخصصی</span>
+    <span>محمدرضا توسلی</span>
+    <span>${time} ۱۸:۳۰−۱۷</span>
+    <span><a href='http://79.143.84.27/thl/' class='link'>${goToClass}</a></span>
+  </li>
+  <p>
+  &#8659;
+  پنج‌شنبه
+  &#8659;
+  </p>
+  
+  <li id='application_programming'>
+    <span>برنامه نویسی موبایل</span>
+    <span>مجید شریف ثانوی</span>
+    <span>${time} ۱۳−۱۰</span>
+    <span><a href='http://79.143.84.27/mob1/' class='link'>${goToClass}</a></span>
+  </li>
+
+  <li id='graphic'>
+    <span>محیط های گرافیکی و ...</span>
+    <span>محمدرضا توسلی</span>
+    <span>${time} ۱۹:۳۰−۱۸</span>
+    <span><a href='http://79.143.84.27/mc/' class='link'>${goToClass}</a></span>
+  </li>
+  
+    
+  `;
 }
 
 /*
